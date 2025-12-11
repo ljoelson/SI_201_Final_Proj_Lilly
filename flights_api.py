7f9cbc7f23f5b9995930f3b440688dfaf61c1587
Lillian Joelson <lillianjoelson@Lillians-MacBook-Pro-6.local>
2025-12-11 18:48:04 -0500

    update 2

--- flights_api.py
+++ flights_api.py
@@ -106,7 +106,7 @@ def store_flight_data(db_conn, flights_list):
     
     # Flight delays table
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS FlightDelays (
+        CREATE TABLE IF NOT EXISTS flight_delays (
             delay_id INTEGER PRIMARY KEY AUTOINCREMENT,
             flight_id INTEGER NOT NULL,
             delay_minutes INTEGER,
@@ -140,7 +140,7 @@ def store_flight_data(db_conn, flights_list):
             flight_id = cursor.lastrowid
             
             cursor.execute('''
-                INSERT INTO FlightDelays (flight_id, delay_minutes)
+                INSERT INTO flight_delays (flight_id, delay_minutes)
                 VALUES (?, ?)
             ''', (flight_id, flight['delay_minutes']))
             
